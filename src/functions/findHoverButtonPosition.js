c02c1fc5e7379de5a1028122dc2185474b09021b
amal jose <howareyouami@gmail.com>
2019-08-02 16:33:16 +0400

    Adding dist

--- src/functions/findHoverButtonPosition.js
+++
@@ -1,7 +0,0 @@
-import { findSelectButtonPosition } from "./findSelectButtonPosition";
-export const findHoverButtonPosition = (hoveredId, highlightData) => {
-  const selectedData = highlightData.find(function(e) {
-    return e.id == hoveredId;
-  });
-  return findSelectButtonPosition(selectedData.position);
-};
